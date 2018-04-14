3a33fa5f423f62f7db2fbb7d197b84173b723c42
Wight554 <wight554@gmail.com>
2018-04-14 14:38:05 +0000

    msm8998-common: add power-libperfmgr dt2w implementation
    * import sysfs_write from wahoo power@1.2
    * hardcode node since to keep blueprint

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -213,7 +213,6 @@ TARGET_USES_MKE2FS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
 # QCOM
