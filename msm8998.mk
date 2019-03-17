5e627972d19b1747e01490a90b1310a93ea95b5a
Verevka <verevka86@gmail.com>
2019-03-17 13:36:05 +0300

    msm8998-common: Build binderized android.hardware.media.omx@1.0-impl

--- msm8998.mk
+++ msm8998.mk
@@ -281,6 +281,8 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service \
     libaacwrapper \
     libc2dcolorconvert \
     libmm-omxcore \
